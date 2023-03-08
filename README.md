0886a3ca92f9d8f0b013bd175f0a255946921b59
jaygala <jaygala260@gmail.com>
2023-03-08 10:57:11 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Trying to understand Vercel serverless functions by making a new bot on Telegram (not twitter because still haven't got access to the developer account on Twitter)
---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+import os
+from threading import Thread
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_BOT_KEY = os.getenv('TELEGRAM_BOT_KEY')
+
+bot = telebot.TeleBot(TELEGRAM_BOT_KEY)
+
+print(bot)
+
+@bot.message_handler(commands = ['hello'])
+def greet(message):
+    bot.send_message(message.chat.id, "Hello")
+
+bot.polling()
